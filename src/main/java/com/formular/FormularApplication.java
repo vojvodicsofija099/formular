ae20b27b1f1432061658d23e09e9ddfe40d047e4
vojvodicsofija099 <vojvodicsofija099@gmail.com>
2024-11-25 11:57:51 +0100

    Initialize Spring Boot app

---
+++ src/main/java/com/formular/FormularApplication.java
@@ -0,0 +1,13 @@
+package com.formular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularApplication.class, args);
+	}
+
+}
